7ab579e0e011e08e437150f50b5f2b3d9bf24805
A10001986 <76924199+realA10001986@users.noreply.github.com>
2024-03-14 19:33:56 +0100

    Minor changes

--- sid-DMX/sid_global.h
+++ sid-DMX/sid_global.h
@@ -13,14 +13,21 @@
  ***                          Version Strings                          ***
  *************************************************************************/
 
-#define SID_VERSION "V0.93"
-#define SID_VERSION_EXTRA "MAR082024"
+#define SID_VERSION "V0.94"
+#define SID_VERSION_EXTRA "MAR092024"
+
+/*************************************************************************
+ ***                        Build configuration                        ***
+ *************************************************************************/
 
 //#define SID_DBG              // debug output on Serial
 
 // If this is uncommented, the firmware uses channel DMX_VERIFY_CHANNEL
 // for packet verification. The value of this channel must, at all times,
 // be DMX_VERIFY_VALUE for a packet to be accepted.
+// Must be disabled (commented) if the DMX controller's blackout function 
+// is to be used but lacks a way to exclude channels (like in case of 
+// QLC+ version 4.x)
 //#define DMX_USE_VERIFY
 
 // Mode for "Effect ramp up" slider at DMX values 1 through 255:
@@ -32,10 +39,10 @@
 //    (30-88mph; strict; including slight randomization up 75mph)
 // 4: like 3, but non-strict
 //
-// For 1 and 3: LEDs are slightly randomized at 2Hz; if slider level 
+// For 1 and 3: Levels are slightly randomized at 2Hz; if slider level 
 // is beyond 75mph (=DMX value 215 in mode 1, and 200 in mode 3), 
 // no more randomization is performed, since remaining steps are
-// meant to resemble the authentic time travel sequence.
+// meant to resemble the authentic linear time travel sequence.
 #define ERU_MODE 3
 
 
@@ -43,16 +50,16 @@
  ***                             GPIO pins                             ***
  *************************************************************************/
 
-// IR Remote
+// IR Remote (unused in DMX version)
 #define IRREMOTE_PIN      27
 
 // IR feedback
 #define IR_FB_PIN         17
 
-// Time Travel button (or TCD input trigger)
+// Time Travel button (or TCD input trigger) (unused in DMX version)
 #define TT_IN_PIN         13
 
-// I2S audio pins
+// I2S audio pins (unused in DMX version)
 #define I2S_BCLK_PIN      26
 #define I2S_LRCLK_PIN     25
 #define I2S_DIN_PIN       33
@@ -64,8 +71,8 @@
 #define SPI_SCK_PIN       18 
 
 // DMX
-#define DMX_TRANSMIT 14
-#define DMX_RECEIVE  35 //13
-#define DMX_ENABLE   32
+#define DMX_TRANSMIT      14
+#define DMX_RECEIVE       35
+#define DMX_ENABLE        32
 
 #endif
